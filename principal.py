8ec65da2a4192513ea345a3bb59b129ac49980f0
ViniciusVonAhn <viniciusvonahn@gmail.com>
2018-09-19 10:47:52 -0300

    testando travis

---
+++ principal.py
@@ -0,0 +1,16 @@
+def dobro(x):
+    """
+       Funcao que retorna o dobro do valor passado como argumento.
+       >>> dobro(1)
+       2
+       >>> dobro(2)
+       4
+       >>> dobro(3)
+       6
+       >>> dobro(4)
+       8
+       >>> dobro(5)
+       10
+       """
+    result = x * 2
+    return result
