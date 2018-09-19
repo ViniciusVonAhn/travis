96692dcc651de95bd475f1d61446195899d7c4c9
ViniciusVonAhn <viniciusvonahn@gmail.com>
2018-09-19 11:05:43 -0300

    teste

--- principal.py
+++ principal.py
@@ -1,16 +1,3 @@
-def dobro(x):
-    """
-       Funcao que retorna o dobro do valor passado como argumento.
-       >>> dobro(1)
-       2
-       >>> dobro(2)
-       4
-       >>> dobro(3)
-       6
-       >>> dobro(4)
-       8
-       >>> dobro(5)
-       10
-       """
-    result = x * 2
-    return result
+def somar(x, y):
+    """docstring for somar"""
+    return(x * y)
